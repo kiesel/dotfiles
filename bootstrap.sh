8b47866093822062774a6049aa6c9790e8043d55
Alex D'Andrea <alex.dandrea@1und1.de>
2015-12-13 10:37:13 +0100

    Add --batch

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,15 @@ if [ ! -d .homesick/repos/homeshick ]; then
 fi
 
 if [ ! -d .homesick/repos/dotfiles ]; then
-  .homesick/repos/homeshick/bin/homeshick clone kiesel/dotfiles
+  .homesick/repos/homeshick/bin/homeshick clone kiesel/dotfiles --batch
 fi
 
 if [ ! -d .homesick/repos/zgen ]; then
-  .homesick/repos/homeshick/bin/homeshick clone tarjoilija/zgen
+  .homesick/repos/homeshick/bin/homeshick clone tarjoilija/zgen --batch
 fi
 
 if [ ! -d .homesick/repos/oh-my-zsh ]; then
-  .homesick/repos/homeshick/bin/homeshick clone robbyrussel/oh-my-zsh
+  .homesick/repos/homeshick/bin/homeshick clone robbyrussel/oh-my-zsh --batch
 fi
 
 .homesick/repos/homeshick/bin/homeshick link --batch

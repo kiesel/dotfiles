b3d9118fb6750356a877848aa2a0d8fbf516dade
Alex D'Andrea <alex.dandrea@1und1.de>
2015-12-13 08:25:35 +0100

    Make idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,18 @@
 set -e -u 
 
 cd $HOME
-git clone git://github.com/andsens/homeshick.git --depth=10
 
-.homesick/repos/homeshick/bin/homeshick clone kiesel/dotfiles
-.homesick/repos/homeshick/bin/homeshick clone tarjoilija/zgen
+if [ ! -d .homesick/repos/homeshick ]; then
+  git clone git://github.com/andsens/homeshick.git .homesick/repos/homeshick --depth=10
+fi
+
+if [ ! -d .homesick/repos/dotfiles ]; then
+  .homesick/repos/homeshick/bin/homeshick clone kiesel/dotfiles
+fi
+
+if [ ! -d .homesick/repos/zgen ]; then
+  .homesick/repos/homeshick/bin/homeshick clone tarjoilija/zgen
+fi
 
 .homesick/repos/homeshick/bin/homeshick link --batch
 

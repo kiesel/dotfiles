bff4be73344e8c00d5b4021d0672fb3ecb238692
Alex D'Andrea <alex.dandrea@1und1.de>
2015-12-13 11:43:33 +0100

    Use git://

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,15 @@ if [ ! -d .homesick/repos/homeshick ]; then
 fi
 
 if [ ! -d .homesick/repos/dotfiles ]; then
-  .homesick/repos/homeshick/bin/homeshick clone kiesel/dotfiles --batch
+  .homesick/repos/homeshick/bin/homeshick clone git://github.com/kiesel/dotfiles --batch
 fi
 
 if [ ! -d .homesick/repos/zgen ]; then
-  .homesick/repos/homeshick/bin/homeshick clone tarjoilija/zgen --batch
+  .homesick/repos/homeshick/bin/homeshick clone git://github.com/tarjoilija/zgen --batch
 fi
 
 if [ ! -d .homesick/repos/oh-my-zsh ]; then
-  .homesick/repos/homeshick/bin/homeshick clone robbyrussel/oh-my-zsh --batch
+  .homesick/repos/homeshick/bin/homeshick clone git://github.com/robbyrussell/oh-my-zsh.git  --batch
 fi
 
 .homesick/repos/homeshick/bin/homeshick link --batch
